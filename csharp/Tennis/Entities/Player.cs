b68fa132aae27d323df3b91d4e9fb63f105a2c05
Carlos Roberto de Paula Junior <carlos.roberto@farfetch.com>
2021-09-26 12:55:09 +0100

    add behavior to Player class

--- csharp/Tennis/Entities/Player.cs
+++ csharp/Tennis/Entities/Player.cs
@@ -3,10 +3,17 @@
     public class Player
     {
         private readonly string _name;
+        public int Point { get; private set; }
 
         public Player(string name)
         {
             _name = name;
+            Point = 0;
+        }
+
+        public void Score()
+        {
+            Point++;
         }
 
         public override string ToString()
--- csharp/Tennis/Entities/TennisGameRefactored.cs
+++ csharp/Tennis/Entities/TennisGameRefactored.cs
@@ -4,98 +4,101 @@ namespace Tennis.Entities
 {
     public class TennisGameRefactored : ITennisGame
     {
-        private int _p1Point;
-        private int _p2Point;
-
+        private readonly Player _p1;
+        private readonly Player _p2;
+        // private int _p1Point;
+        // private int _p2.Point;
+        //
         private string _p1Res = "";
         private string _p2Res = "";
 
-        public TennisGameRefactored()
+        public TennisGameRefactored(Player p1, Player p2)
         {
-            _p1Point = 0;
+            _p1 = p1;
+            _p2 = p2;
         }
 
         public string GetScore()
         {
             var score = "";
-            if (_p1Point == _p2Point && _p1Point < 3)
+            if (_p1.Point == _p2.Point && _p1.Point < 3)
             {
-                if (_p1Point == 0)
+                if (_p1.Point == 0)
                     score = "Love";
-                if (_p1Point == 1)
+                if (_p1.Point == 1)
                     score = "Fifteen";
-                if (_p1Point == 2)
+                if (_p1.Point == 2)
                     score = "Thirty";
                 score += "-All";
             }
-            if (_p1Point == _p2Point && _p1Point > 2)
+            if (_p1.Point == _p2.Point && _p1.Point > 2)
                 score = "Deuce";
 
-            if (_p1Point > 0 && _p2Point == 0)
+            if (_p1.Point > 0 && _p2.Point == 0)
             {
-                if (_p1Point == 1)
+                if (_p1.Point == 1)
                     _p1Res = "Fifteen";
-                if (_p1Point == 2)
+                if (_p1.Point == 2)
                     _p1Res = "Thirty";
-                if (_p1Point == 3)
+                if (_p1.Point == 3)
                     _p1Res = "Forty";
 
                 _p2Res = "Love";
                 score = _p1Res + "-" + _p2Res;
             }
-            if (_p2Point > 0 && _p1Point == 0)
+            if (_p2.Point > 0 && _p1.Point == 0)
             {
-                if (_p2Point == 1)
+                if (_p2.Point == 1)
                     _p2Res = "Fifteen";
-                if (_p2Point == 2)
+                if (_p2.Point == 2)
                     _p2Res = "Thirty";
-                if (_p2Point == 3)
+                if (_p2.Point == 3)
                     _p2Res = "Forty";
 
                 _p1Res = "Love";
                 score = _p1Res + "-" + _p2Res;
             }
 
-            if (_p1Point > _p2Point && _p1Point < 4)
+            if (_p1.Point > _p2.Point && _p1.Point < 4)
             {
-                if (_p1Point == 2)
+                if (_p1.Point == 2)
                     _p1Res = "Thirty";
-                if (_p1Point == 3)
+                if (_p1.Point == 3)
                     _p1Res = "Forty";
-                if (_p2Point == 1)
+                if (_p2.Point == 1)
                     _p2Res = "Fifteen";
-                if (_p2Point == 2)
+                if (_p2.Point == 2)
                     _p2Res = "Thirty";
                 score = _p1Res + "-" + _p2Res;
             }
-            if (_p2Point > _p1Point && _p2Point < 4)
+            if (_p2.Point > _p1.Point && _p2.Point < 4)
             {
-                if (_p2Point == 2)
+                if (_p2.Point == 2)
                     _p2Res = "Thirty";
-                if (_p2Point == 3)
+                if (_p2.Point == 3)
                     _p2Res = "Forty";
-                if (_p1Point == 1)
+                if (_p1.Point == 1)
                     _p1Res = "Fifteen";
-                if (_p1Point == 2)
+                if (_p1.Point == 2)
                     _p1Res = "Thirty";
                 score = _p1Res + "-" + _p2Res;
             }
 
-            if (_p1Point > _p2Point && _p2Point >= 3)
+            if (_p1.Point > _p2.Point && _p2.Point >= 3)
             {
                 score = "Advantage player1";
             }
 
-            if (_p2Point > _p1Point && _p1Point >= 3)
+            if (_p2.Point > _p1.Point && _p1.Point >= 3)
             {
                 score = "Advantage player2";
             }
 
-            if (_p1Point >= 4 && _p2Point >= 0 && (_p1Point - _p2Point) >= 2)
+            if (_p1.Point >= 4 && _p2.Point >= 0 && (_p1.Point - _p2.Point) >= 2)
             {
                 score = "Win for player1";
             }
-            if (_p2Point >= 4 && _p1Point >= 0 && (_p2Point - _p1Point) >= 2)
+            if (_p2.Point >= 4 && _p1.Point >= 0 && (_p2.Point - _p1.Point) >= 2)
             {
                 score = "Win for player2";
             }
@@ -120,12 +123,12 @@ namespace Tennis.Entities
 
         private void P1Score()
         {
-            _p1Point++;
+            _p1.Score();
         }
 
         private void P2Score()
         {
-            _p2Point++;
+            _p2.Score();
         }
 
         public void WonPoint(string player)
