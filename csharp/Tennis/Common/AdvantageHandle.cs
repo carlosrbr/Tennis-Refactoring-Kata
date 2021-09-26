acf2332014bf45643c58811b822e4e384b776e99
Carlos Roberto de Paula Junior <carlos.roberto@farfetch.com>
2021-09-26 19:28:02 +0100

    added interpolation to a string.

--- csharp/Tennis/Common/AdvantageHandle.cs
+++ csharp/Tennis/Common/AdvantageHandle.cs
@@ -4,16 +4,16 @@ namespace Tennis.Common
 {
     public class AdvantageHandle : AbstractHandler
     {
-        public override string Handle(Player player1,Player player2)
+        public override string Handle(Player player1, Player player2)
         {
             if (player1.Point > player2.Point && player2.Point >= 3)
             {
-                return "Advantage player1";
+                return $"Advantage {player1}";
             }
 
             if (player2.Point > player1.Point && player1.Point >= 3)
             {
-                return "Advantage player2";
+                return $"Advantage {player2}";
             }
 
             return base.Handle(player1, player2);
